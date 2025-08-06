667adf9e32ab35a16dd479958c16c95160d3929f
densumesh <dens.sumesh79@gmail.com>
2025-08-05 22:24:09 -0700

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://densumesh.dev',
   integrations: [mdx(), react()],
-  output: 'static'
+  output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
